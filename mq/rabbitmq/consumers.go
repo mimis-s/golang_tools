f65fd0c6825f1b861410c5b45364670c4cfcc58d
zhangbin <zhangbin@joynova.com.cn>
2022-11-09 15:04:04 +0800

    修改mq消费者逻辑

--- mq/rabbitmq/consumers.go
+++ mq/rabbitmq/consumers.go
@@ -13,64 +13,76 @@ type consumers struct {
 	queueName string
 }
 
-func InitConsumers(url, exchangeName, routingKey string) (*consumers, error) {
-	conn, err := amqp.Dial(url)
-	if err != nil {
-		return nil, fmt.Errorf("连接mq错误:%v", err)
-	}
-
-	ch, err := conn.Channel()
-	if err != nil {
-		return nil, fmt.Errorf("打开channel错误:%v", err)
-	}
-
-	err = ch.ExchangeDeclare(
-		exchangeName, // name
-		"topic",      // type
-		true,         // durable
-		false,        // auto-deleted
-		false,        // internal
-		false,        // no-wait
-		nil,          // arguments
-	)
-	if err != nil {
-		return nil, fmt.Errorf("定义exchange错误:%v", err)
-	}
-
-	// 消费者队列
-	q, err := ch.QueueDeclare(
-		"",    // name
-		false, // durable
-		false, // delete when usused
-		true,  // exclusive
-		false, // no-wait
-		nil,   // arguments
-	)
-	if err != nil {
-		return nil, fmt.Errorf("定义queue错误:%v", err)
-	}
-
-	// 绑定队列到exchange，通过routing路由到消费者
-	err = ch.QueueBind(
-		q.Name,
-		routingKey,
-		exchangeName,
-		false,
-		nil,
-	)
-	if err != nil {
-		return nil, fmt.Errorf("queue[%v]绑定exchange[%v]错误:%v", q.Name, exchangeName, err)
-	}
-
-	return &consumers{ch, q.Name}, nil
+// 消费者队列
+type ConsumersQueue struct {
+	ExchangeName string
+	RoutingKey   string
+	CallBack     func(payload interface{}) error
 }
 
-func (c *consumers) RegisterConsume(callBack func(payload interface{}) error) error {
+func RegisterConsumers(url string, cQueue []*ConsumersQueue) error {
+	conn, err := amqp.Dial(url)
+	if err != nil {
+		return fmt.Errorf("连接mq错误:%v", err)
+	}
+
+	for _, c := range cQueue {
+
+		ch, err := conn.Channel()
+		if err != nil {
+			return fmt.Errorf("打开channel错误:%v", err)
+		}
+
+		err = ch.ExchangeDeclare(
+			c.ExchangeName, // name
+			"topic",        // type
+			true,           // durable
+			false,          // auto-deleted
+			false,          // internal
+			false,          // no-wait
+			nil,            // arguments
+		)
+		if err != nil {
+			return fmt.Errorf("定义exchange错误:%v", err)
+		}
+
+		// 消费者队列
+		q, err := ch.QueueDeclare(
+			"",    // name
+			false, // durable
+			false, // delete when usused
+			true,  // exclusive
+			false, // no-wait
+			nil,   // arguments
+		)
+		if err != nil {
+			return fmt.Errorf("定义queue错误:%v", err)
+		}
+
+		// 绑定队列到exchange，通过routing路由到消费者
+		err = ch.QueueBind(
+			q.Name,
+			c.RoutingKey,
+			c.ExchangeName,
+			false,
+			nil,
+		)
+		if err != nil {
+			return fmt.Errorf("queue[%v]绑定exchange[%v]错误:%v", q.Name, c.ExchangeName, err)
+		}
+
+		go consume(ch, q.Name, c.CallBack)
+	}
+
+	return nil
+}
+
+func consume(ch *amqp.Channel, queueName string, callBack func(payload interface{}) error) error {
 
 	errChannel := make(chan *amqp.Error)
 
-	msgs, err := c.channel.Consume(
-		c.queueName,
+	msgs, err := ch.Consume(
+		queueName,
 		"",
 		true,
 		false,
@@ -81,7 +93,7 @@ func (c *consumers) RegisterConsume(callBack func(payload interface{}) error) er
 	if err != nil {
 		return fmt.Errorf("接收mq消息错误:%v", err)
 	}
-	c.channel.NotifyClose(errChannel)
+	ch.NotifyClose(errChannel)
 
 	go func() {
 		for d := range msgs {
@@ -99,6 +111,7 @@ func (c *consumers) RegisterConsume(callBack func(payload interface{}) error) er
 	select {
 	case mqErr := <-errChannel:
 		return mqErr
+	default:
+		return nil
 	}
-
 }
