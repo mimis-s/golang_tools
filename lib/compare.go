19eb1a56bf934825bc795aab15067f14a9cb0f28
zhangbin <zhangbin@joynova.com.cn>
2023-01-03 15:36:16 +0800

    int64添加大小对比

--- lib/compare.go
+++ lib/compare.go
@@ -41,3 +41,17 @@ func MinInt(a, b int) int {
 	}
 	return b
 }
+
+func MaxInt64(a, b int64) int64 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func MinInt64(a, b int64) int64 {
+	if a < b {
+		return a
+	}
+	return b
+}
